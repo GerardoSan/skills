5f1ef3ca3a3c3e9ef5c05e067ca680194eb9f0cc
enriquesanchez08 <enrique.sanchez@manantial.pe>
2026-04-27 11:27:14 -0500

    Fix quota issues: reduce nodes and use standard disk

--- terraform/GCP/GKE-OIDC/terraform/main.tf
+++ terraform/GCP/GKE-OIDC/terraform/main.tf
@@ -34,10 +34,12 @@ resource "google_container_node_pool" "primary_nodes" {
   name       = "default-node-pool"
   location   = var.region
   cluster    = google_container_cluster.primary.name
-  node_count = 2
+  node_count = 1
 
   node_config {
     machine_type = "e2-medium"
+    disk_type    = "pd-standard"
+    disk_size_gb = 20
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
     ]
